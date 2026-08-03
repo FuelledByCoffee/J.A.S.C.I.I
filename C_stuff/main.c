62d9c506d96873ab71736d2913bc4a0d4cadb1f8
Jaskaran Singh <jassi.dev@icloud.com>
2026-08-03 14:25:21 +0530

    imp stuff
    resolution was increased and stuff like comments, a readme were added

--- C_stuff/main.c
+++ C_stuff/main.c
@@ -1,59 +1,58 @@
+// welcome welcome, reading my shitty code are we xD, anyways keep going down. I will try my best to explain stuff :)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #ifdef __EMSCRIPTEN__
   #include <emscripten.h>
 #else
   #define EMSCRIPTEN_KEEPALIVE
 #endif
-
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION       // ok so like line 2-15 just import the stuff needed such as Stb libraries for processing and resizing images and ofc stdio.h the goat cant even print without it lol
 #include "stb_image_resize2.h"
 
 int width_shrunk;
-int height_shrunk;
+int height_shrunk; 
 
 int main (void){
-  int width, height, original_channels;
+  int width, height, original_channels;  //ah yes, initialising some *kewl* variables :)
   int x = 0;
   int y = 0;
   int i = 0;
-  char ASCIIMAP[29] = "N@#W$9876543210?!abc;:+=-,._ ";
+  char ASCIIMAP[29] = "N@#W$9876543210?!abc;:+=-,._ ";    //i stole this online
   int num_char = strlen(ASCIIMAP);
   int index;
   int img_index;
   unsigned char *img = stbi_load(
-    "googoo.png",
+    "googoo.png",       //i am not a baby, its from this video: https://youtube.com/shorts/1MCfhbbxogQ?si=rioc5poo844f9C3y | walt disney, nike, *GOOGOO*
     &width, &height, &original_channels, 3
   );
-  width_shrunk = width;
-  height_shrunk = height;
-  while (width_shrunk > 40 && height_shrunk > 40) {
+  width_shrunk = width;   //this is because uhhhh idk
+  height_shrunk = height; //no actually this is so that we can shrink them with the algorithm (or whatever the fuck you call it) below
+  while (width_shrunk > 60 && height_shrunk > 60) {
     width_shrunk = width_shrunk * 0.95;
-    height_shrunk = height_shrunk * 0.95;
+    height_shrunk = height_shrunk * 0.95;       // this is smart right ( ˊᵕˋ )
   }
-  width_shrunk = width_shrunk * 2;
-  unsigned char *img_resize = malloc(width_shrunk * height_shrunk * 3);
+  width_shrunk = width_shrunk * 2;    //this is because terminal likes to shrink the width to roughly half of height (shoutout bolt from cs50 discord)
+  unsigned char *img_resize = malloc(width_shrunk * height_shrunk * 3);   //this is to reserve data in heap so that my program does not kill itself lol
   float brightness;
   if (img == NULL) {
     printf("failure\n");
-    free(img_resize);
+    free(img_resize);     //ahh yes i dont wanna take up your ram
     free(img);
     return 1;
   }
   else {
-    stbir_resize_uint8_srgb(img, width, height, 0, img_resize, width_shrunk, height_shrunk, 0, STBIR_RGB);
+    stbir_resize_uint8_srgb(img, width, height, 0, img_resize, width_shrunk, height_shrunk, 0, STBIR_RGB);    //stb the goat
   }
   printf("\n");
   while (y != height_shrunk) {
     while (x != width_shrunk) {
       img_index = (((y * width_shrunk) + x) * 3);
-      brightness = ((img_resize[img_index + 0] * 0.2126) + (img_resize[img_index + 1] * 0.7152) + (img_resize[img_index + 2] * 0.0722));
+      brightness = ((img_resize[img_index + 0] * 0.2126) + (img_resize[img_index + 1] * 0.7152) + (img_resize[img_index + 2] * 0.0722));    //basically the entire ascii thing, so like till one y is done it keeps doing the x axis so like imagine (0,0) so it will keep increasing x by one until width is reached then it increases y by one and resets x. also it prints every pixel with a character based of brightness using luma formula
       index = (int)((brightness * (num_char - 1))/255);
       printf("%c", ASCIIMAP[index]);
       x++;
@@ -64,15 +63,17 @@ int main (void){
     y++;
   }
   free(img_resize);
-  free(img);
+  free(img);      //no ram hogging in here :D
 }
 
 EMSCRIPTEN_KEEPALIVE
 int image_width() {
-    return width_shrunk;
+    return width_shrunk;    //this is to give the data to emscripten so that it can resize the div
 }
 
 EMSCRIPTEN_KEEPALIVE
 int image_height() {
-    return height_shrunk;
+    return height_shrunk;  //same as above
 }
+
+//i hope you did not find my comments annoying, jassi out :)
---
+++ README.md
@@ -0,0 +1,21 @@
+wow first readme, kinda nervous.
+> NOTE: line 56-79 in /frontend/index.html was written with the help of an llm (i am sorry :c)
+# J.A.S.C.I.I
+> jassi's image (maybe even videos later c: ) to ascii art program, it uses C for the ascii generation and *web technology* for the front end
+[its up rn!!! go to it :)](https://jascii.jassi.dev/)
+
+## Some Examples
+(rem from rezero, i am sorry idk where the art is from) <br>
+![wow you could not load the image lol](rem.jpg)
+![is your internet really that bad?](rem_ASCII.webp) <br>
+(mizuki akiyama from project sekai, image is from: https://projectsekai.fandom.com/wiki/Akiyama_Mizuki?file=Mizuki+3rd+Anniversary.png) <br>
+![gng get a better router :sob:](mizuki_akiyama.png)
+![bad internet lolllll](mizuk_akiyama_ASCII.png) <br>
+### Why should i use this?
+umm because:
+1. its free :)
+2. your images (and potentially videos in the future) are not going to some random data base so that zuckerberg can feed it to meta
+3. its freeeeeeeeeeeeee :D
+4. why not 
+> credit to sophie for helping with wasm implementation <br>
+thats it, jassi out :)
