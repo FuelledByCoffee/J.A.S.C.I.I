03b753bd0824ee468bc123c4cbc74e55fad3b63e
Jaskaran Singh <jassi.dev@icloud.com>
2026-08-08 12:25:48 +0530

    removed uncompiled css
    now the build.sh will auto uncompile the tailwind when ran

--- build.sh
+++ build.sh
@@ -16,6 +16,7 @@ cd ..
 npm install
 cd frontend/
 npm install
+npx tailwindcss -i ./input.css -o ./output.css --minify
 echo "Go to http://localhost:6969/ <-- yes 6969 very funni number :)"
 node server.js
 
