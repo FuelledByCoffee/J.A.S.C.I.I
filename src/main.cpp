62bb3522fd0341d582a7ad5cf65ee67bf3909e99
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-08-12 19:55:40 +0200

    Add function for load_image

--- src/main.cpp
+++ src/main.cpp
@@ -42,10 +42,26 @@ static constexpr auto changeSaturation(pixel p, double change) -> pixel {
 	                      G * G * Pg + //
 	                      B * B * Pb);
 
-	p.red   = std::fmin(std::fabs(P + ((R)-P) * change), 255);
-	p.green = std::fmin(std::fabs(P + ((G)-P) * change), 255);
-	p.blue  = std::fmin(std::fabs(P + ((B)-P) * change), 255);
+	p.red   = std::min(std::fabs(P + ((R)-P) * change), 255.);
+	p.green = std::min(std::fabs(P + ((G)-P) * change), 255.);
+	p.blue  = std::min(std::fabs(P + ((B)-P) * change), 255.);
 	return p;
+}
+
+// -----------------------------------------------------------------------------
+static auto load_image(std::string_view image_path) {
+	int                      width, height, original_channels; // NOLINT
+	std::unique_ptr<stbi_uc> img{
+			stbi_load(image_path.data(), &width, &height, &original_channels, 3)};
+	if (!img) errx(2, "Failed to load image %s", image_path.data());
+
+	const double scale_factor = size / std::fmax(height, width);
+	width_shrunk            = width * scale_factor * 2; // Thanks bolt!
+	height_shrunk           = height * scale_factor;
+
+	return std::unique_ptr<pixel>(reinterpret_cast<pixel *>(stbir_resize(
+			img.get(), width, height, 0, nullptr, width_shrunk, height_shrunk, 0,
+			STBIR_RGB, STBIR_TYPE_UINT8, STBIR_EDGE_ZERO, STBIR_FILTER_BOX)));
 }
 
 // -----------------------------------------------------------------------------
@@ -71,41 +87,25 @@ int main(int argc, char **argv) {
 		}
 	}
 	if (optind < argc) image_path = argv[optind];
+	if (!image_path) errx(1, "Need an image");
 
 	constexpr char ASCIIMAP[] = "N@#W$9876543210?!abc;:+=-_,.  ";
 	constexpr int  num_char   = sizeof ASCIIMAP - 1;
 
-	int                    width, height, original_channels; // NOLINT
-	std::unique_ptr<pixel> img{reinterpret_cast<pixel *>(
-			stbi_load(image_path, &width, &height, &original_channels, 3))};
-	if (!img) errx(1, "Failed to load image %s", image_path);
-
-	const double scale_factor = size / fmax(height, width);
-	width_shrunk              = (int)(width * scale_factor * 2);
-	height_shrunk             = (int)(height * scale_factor);
-
-
-	auto map = std::make_unique<pixel[]>(width_shrunk * height_shrunk);
-	stbir_resize(img.get(), width, height, 0, map.get(), width_shrunk,
-	             height_shrunk, 0, STBIR_RGB, STBIR_TYPE_UINT8, STBIR_EDGE_ZERO,
-	             STBIR_FILTER_BOX); // stb the goat
+	const auto map = load_image(image_path);
 
 	for (int y = 0; y != height_shrunk; y++) {
 		for (int x = 0; x != width_shrunk; x++) {
 			const int   i = y * width_shrunk + x;
-			const pixel p = changeSaturation(map[i], 1.5);
+			const pixel p = changeSaturation(map.get()[i], 1.5);
 
 			const double brightness = p.red * 0.2126 +   //
 			                          p.green * 0.7152 + //
 			                          p.blue * 0.0722;
 			const int index = brightness * (num_char - 1) / 255;
 			if (color)
-
-				std::cout << "\033[38;2;"                     //
-									<< static_cast<int>(p.red) << ";"   //
-									<< static_cast<int>(p.green) << ";" //
-									<< static_cast<int>(p.blue) << "m"  //
-									<< ASCIIMAP[index] << "\033[0m";
+				std::cout << std::format("\033[38;2;{:d};{:d};{:d}m{}\033[0m", //
+				                         p.red, p.green, p.blue, ASCIIMAP[index]);
 			else std::cout << ASCIIMAP[index];
 		}
 
@@ -113,9 +113,9 @@ int main(int argc, char **argv) {
 	}
 }
 
-extern "C" {
 // -----------------------------------------------------------------------------
 // this is to give the data to emscripten so that it can resize the div
+extern "C" {
 EMSCRIPTEN_KEEPALIVE int  image_width() { return width_shrunk; }
 EMSCRIPTEN_KEEPALIVE int  image_height() { return height_shrunk; }
 EMSCRIPTEN_KEEPALIVE void set_size(int a) { size = a; }
