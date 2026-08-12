f003ec86a6a54adb57ab81276e3d953487915858
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-08-12 09:18:25 +0200

    Working for now
    Updated test to work with wsl

--- C_stuff/main.c
+++ C_stuff/main.c
@@ -1,41 +1,31 @@
-// welcome welcome, reading my shitty code are we xD, anyways keep going down. I
-// will try my best to explain stuff :)
 #include <err.h>
+#include <getopt.h>
 #include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <getopt.h>
+#include <unistd.h>
 #ifdef __EMSCRIPTEN__
 	#include <emscripten.h>
 #else
 	#define EMSCRIPTEN_KEEPALIVE
 #endif
-#include "external/stb_image.h"
-#include "external/stb_image_resize2.h"
+#include <stb_image.h>
+#include <stb_image_resize2.h>
 
 #define Pr .299
 #define Pg .587
 #define Pb .114
 
-int size = 60;
-int color;
-
-EMSCRIPTEN_KEEPALIVE
-void get_size(int a)
-{
-	size = a;
-}
-
-EMSCRIPTEN_KEEPALIVE
-void color_set(int z)
-{
-	color = z;
-}
+static int width_shrunk;
+static int height_shrunk;
+static int size  = 60;
+static int color = false;
 
 // stole this code from here: https://alienryderflex.com/saturation.html
-void changeSaturation(double *R, double *G, double *B, double change)
-{
+static void changeSaturation(unsigned *R, unsigned *G, unsigned *B,
+                             double change) {
 	double P = sqrt((*R) * (*R) * Pr + //
 	                (*G) * (*G) * Pg + //
 	                (*B) * (*B) * Pb);
@@ -45,71 +35,55 @@ void changeSaturation(double *R, double *G, double *B, double change)
 	*B = P + ((*B) - P) * change;
 }
 
-int width_shrunk;
-int height_shrunk;
-
-static struct option long_options[] = {
+const struct option long_options[] = {
 		{"version",       no_argument, NULL, 'v'},
 		{  "image", required_argument, NULL, 'i'},
 		{  "color",       no_argument, NULL, 'c'},
 		{   "size", required_argument, NULL, 's'},
-		{		"help", 			no_argument, NULL, 'h'},
-		{     NULL,				 0, NULL,   0}
+		{   "help",       no_argument, NULL, 'h'},
+		{		 NULL,								 0, NULL,   0}
 };
 
-int main(int argc, char **argv)
-{
-	int         opt        = 0;
-	int         opt_index  = 0;
-	const char *image_path = "googoo.png";
+int main(int argc, char **argv) {
+	int         opt = 0;
+	// int         opt_index  = 0;
+	const char *image_path = argv[1];
+	while ((opt = getopt(argc, argv, "vi:cs:h")) != -1) {
+		switch (opt) {
+		case 'c':
+			color = 1;
+			// Both --color and -c are handled here
+			break;
+		case 'i': image_path = optarg; break;
+		case 's': size = atoi(optarg); break;
+		default: break;
+		}
+	}
 
-if (argc > 1) {
-    if (strcmp(argv[1], "-h") == 0) {
-        printf("./ascii <file location> <color (1 for yes, 0 for no)> <size>\n");
-        exit(0);
-    }
-    image_path = argv[1];
-}
+	image_path = image_path ? image_path : "googoo.png";
 
-if (argc > 2) {
-    color = atoi(argv[2]);
-    if (color < 0 || color > 1) exit(1); // Color must be 0 or 1
-}
-
-if (argc > 3) {
-    size = atoi(argv[3]);
-    if (size <= 0) exit(1); // Size must be a valid positive number
-}
-	// i stole this online
 	const char ASCIIMAP[] = "N@#W$9876543210?!abc;:+=-_,.  ";
 	const int  num_char   = sizeof ASCIIMAP - 1;
 
-	// ah yes, initialising some *kewl* variables :)
-	int      width, height, original_channels;
-	stbi_uc *img =
-			stbi_load(image_path, &width, &height, &original_channels, 3);
+	int      width, height, original_channels; // NOLINT
+	stbi_uc *img = stbi_load(image_path, &width, &height, &original_channels, 3);
 	if (!img) errx(1, "Failed to load image %s", image_path);
 
-	// this is because terminal likes to shrink the width to roughly half of
-	// height (shoutout bolt from cs50 discord)
 	const double scale_factor = size / fmax(height, width);
 	width_shrunk              = (int)(width * scale_factor * 2);
 	height_shrunk             = (int)(height * scale_factor);
 
-	stbi_uc *map = malloc(width_shrunk * height_shrunk * 3);
-	// stbir_resize_uint8_srgb(img, width, height, 0, map, width_shrunk,
-	// height_shrunk, 0, STBIR_RGB); // stb the goat
+	stbi_uc *map = malloc((long)width_shrunk * height_shrunk * 3UL);
 	stbir_resize(img, width, height, 0, map, width_shrunk, height_shrunk, 0,
 	             STBIR_RGB, STBIR_TYPE_UINT8, STBIR_EDGE_ZERO,
 	             STBIR_FILTER_BOX); // stb the goat
 
-	printf("\n");
 	for (int y = 0; y != height_shrunk; y++) {
 		for (int x = 0; x != width_shrunk; x++) {
-			int    i = ((y * width_shrunk) + x) * 3;
-			double r = map[i + 0];
-			double g = map[i + 1];
-			double b = map[i + 2];
+			int      i = ((y * width_shrunk) + x) * 3;
+			unsigned r = map[i + 0];
+			unsigned g = map[i + 1];
+			unsigned b = map[i + 2];
 			changeSaturation(&r, &g, &b, 1.5);
 
 			// what if change saturation causes some weird shit, this is to fix
@@ -130,9 +104,9 @@ if (argc > 3) {
 				b = 0;
 			}
 
-			float brightness = r * 0.2126 + g * 0.7152 + b * 0.0722;
-			int   index      = (int)((brightness * (num_char - 1)) / 255);
-			if (color == 1) // checks if colour was toggled on or off
+			double brightness = r * 0.2126 + g * 0.7152 + b * 0.0722;
+			int    index      = (int)((brightness * (num_char - 1)) / 255);
+			if (color) // checks if colour was toggled on or off
 			{
 				printf("\033[38;2;%d;%d;%dm%c\033[0m", (int)r, (int)g, (int)b,
 				       ASCIIMAP[index]);
@@ -148,16 +122,10 @@ if (argc > 3) {
 }
 
 // this is to give the data to emscripten so that it can resize the div
-EMSCRIPTEN_KEEPALIVE
-int image_width()
-{
-	return width_shrunk;
-}
-
-EMSCRIPTEN_KEEPALIVE
-int image_height()
-{
-	return height_shrunk; // same as above
-}
+EMSCRIPTEN_KEEPALIVE int  image_width() { return width_shrunk; }
+EMSCRIPTEN_KEEPALIVE int  image_height() { return height_shrunk; }
+EMSCRIPTEN_KEEPALIVE void set_size(int a) { size = a; }
+EMSCRIPTEN_KEEPALIVE void set_color(int z) { color = z; }
 
 // i hope you did not find my comments annoying, jassi out :)
+// vim: ts=2
--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-source C_stuff/emsdk/emsdk_env.sh
+source emsdk/emsdk_env.sh
 cd frontend
-emrun index.html
+# emrun index.html
+
+emrun --browser="/mnt/c/Program Files/Google/Chrome/Application/chrome.exe" index.html
+
