f8c24aa8fad732a80e57491571bd2149e342c2e2
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-08-14 13:12:37 +0200

    Don't resize image, find index dynamically.
    Use NTSC brightness formula

--- src/main.cpp
+++ src/main.cpp
@@ -16,19 +16,17 @@
 	#define EMSCRIPTEN_KEEPALIVE
 #endif
 #include <stb_image.h>
-#include <stb_image_resize2.h>
 
-static int width_shrunk;
-static int height_shrunk;
-static int size  = 60;
+static int g_target_width = 160;
+static int g_target_height;
 static int color = false;
 
 // -----------------------------------------------------------------------------
 // this is to give the data to emscripten so that it can resize the div
 extern "C" {
-EMSCRIPTEN_KEEPALIVE int  image_width() { return width_shrunk; }
-EMSCRIPTEN_KEEPALIVE int  image_height() { return height_shrunk; }
-EMSCRIPTEN_KEEPALIVE void set_size(int a) { size = a; }
+EMSCRIPTEN_KEEPALIVE int  image_width() { return g_target_width; }
+EMSCRIPTEN_KEEPALIVE int  image_height() { return g_target_height; }
+EMSCRIPTEN_KEEPALIVE void set_size(int a) { g_target_width = a; }
 EMSCRIPTEN_KEEPALIVE void set_color(int z) { color = z; }
 }
 
@@ -43,13 +41,13 @@ struct pixel {
 template <typename pixel_type = pixel, //
           typename free_func  = decltype(&stbi_image_free)>
 struct image {
-	using ptr = std::unique_ptr<pixel_type, free_func>;
-	image(u8 *data, int height, int width, free_func ff = stbi_image_free)
-		: m_data(reinterpret_cast<pixel_type *>(data), ff), m_height(height),
+	using u_ptr = std::unique_ptr<pixel_type, free_func>;
+	image(u8 *data, int height, int width, free_func deleter = stbi_image_free)
+		: m_data(reinterpret_cast<pixel_type *>(data), deleter), m_height(height),
 			m_width(width) {}
-	ptr m_data;
-	int m_height = 0;
-	int m_width  = 0;
+	u_ptr m_data;
+	int   m_height = 0;
+	int   m_width  = 0;
 
 	pixel_type &operator[](int i) { return m_data.get()[i]; }
 };
@@ -82,68 +80,62 @@ static auto load_image(const char *image_path) {
 	int   width, height, original_channels; // NOLINT
 	auto *img = stbi_load(image_path, &width, &height, &original_channels, 3);
 	if (!img) errx(2, "Failed to load image %s", image_path);
-
-	const double scale_factor = size / std::fmax(height, width);
-	width_shrunk              = width * scale_factor * 2; // Thanks bolt!
-	height_shrunk             = height * scale_factor;
-
-	auto *img_resized = stbir_resize(
-			img, width, height, 0, nullptr, width_shrunk, height_shrunk, 0, STBIR_RGB,
-			STBIR_TYPE_UINT8, STBIR_EDGE_ZERO, STBIR_FILTER_BOX);
-
-	stbi_image_free(img);
-
-	return image((u8 *)img_resized, height, width, stbi_image_free);
+	return image(img, height, width, stbi_image_free);
 }
 
 // -----------------------------------------------------------------------------
 static auto make_ascii_art(auto &&img) {
 	constexpr char ASCIIMAP[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/"
 															"\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
-	constexpr int  num_char   = sizeof ASCIIMAP - 1;
+	constexpr int  num_chars  = sizeof ASCIIMAP - 1;
 
 	std::stringstream art;
+	const double      aspect_ratio           = double(img.m_height) / img.m_width;
+	constexpr double  char_aspect_adjustment = 0.55;
+	g_target_height =
+			(int)(g_target_width * aspect_ratio * char_aspect_adjustment);
 
-	for (int y = 0; y != height_shrunk; y++) {
-		for (int x = 0; x != width_shrunk; x++) {
-			const int   i = y * width_shrunk + x;
-			const pixel p = changeSaturation(img[i], 1.5);
+	for (int y = 0; y != g_target_height; y++) {
+		for (int x = 0; x != g_target_width; x++) {
+			const int   new_x = int(double(x) / g_target_width * img.m_width);
+			const int   new_y = int(double(y) / g_target_height * img.m_height);
+			const int   i     = new_y * img.m_width + new_x;
+			const pixel p     = changeSaturation(img[i], 1.5);
 
-			const double brightness = p.red * 0.2126 +   //
-			                          p.green * 0.7152 + //
-			                          p.blue * 0.0722;
+			const double brightness = 0.299 * p.red +   //
+			                          0.587 * p.green + //
+			                          0.114 * p.blue;
 
-			const unsigned index = unsigned(brightness * (num_char - 1) / 255);
+			const unsigned index = unsigned(brightness / 255.0 * (num_chars - 1));
 			if (color)
 				art << std::format("\033[38;2;{:d};{:d};{:d}m{}\033[0m", //
 				                   p.red, p.green, p.blue, ASCIIMAP[index]);
 			else art << ASCIIMAP[index];
 		}
-
 		art << '\n';
 	}
 	return art.str();
 }
 
 // -----------------------------------------------------------------------------
-static constexpr struct option long_options[] = {
-		{"version",       no_argument, NULL, 'v'},
-		{  "image", required_argument, NULL, 'i'},
-		{  "color",       no_argument, NULL, 'c'},
-		{   "size", required_argument, NULL, 's'},
-		{   "help",       no_argument, NULL, 'h'},
-		{		 NULL,								 0, NULL,   0}
-};
-
-// -----------------------------------------------------------------------------
 int main(int argc, char **argv) {
+
+	constexpr struct option long_options[] = {
+			{"version",       no_argument, NULL, 'v'},
+			{  "image", required_argument, NULL, 'i'},
+			{  "color",       no_argument, NULL, 'c'},
+			{   "size", required_argument, NULL, 's'},
+			{   "help",       no_argument, NULL, 'h'},
+			{     NULL,								 0, NULL,   0}
+  };
+
 	int         opt        = 0;
 	const char *image_path = argv[1];
 	while ((opt = getopt_long(argc, argv, "vi:cs:h", long_options, NULL)) != -1) {
 		switch (opt) {
 			case 'c': color = 1; break;
 			case 'i': image_path = optarg; break;
-			case 's': size = atoi(optarg); break;
+			case 's': g_target_width = atoi(optarg); break;
 			default: break;
 		}
 	}
