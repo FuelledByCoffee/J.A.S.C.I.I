8221c37d29be196c225689d4bc0de033cb8eaa16
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-08-14 10:55:27 +0200

    Introduce image struct longer asciimap and stringstream

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <err.h>
 #include <getopt.h>
 #include <memory>
+#include <sstream>
 #include <unistd.h>
 
 #include <cmath>
@@ -17,6 +18,20 @@
 #include <stb_image.h>
 #include <stb_image_resize2.h>
 
+static int width_shrunk;
+static int height_shrunk;
+static int size  = 60;
+static int color = false;
+
+// -----------------------------------------------------------------------------
+// this is to give the data to emscripten so that it can resize the div
+extern "C" {
+EMSCRIPTEN_KEEPALIVE int  image_width() { return width_shrunk; }
+EMSCRIPTEN_KEEPALIVE int  image_height() { return height_shrunk; }
+EMSCRIPTEN_KEEPALIVE void set_size(int a) { size = a; }
+EMSCRIPTEN_KEEPALIVE void set_color(int z) { color = z; }
+}
+
 using u8 = std::uint8_t;
 
 struct pixel {
@@ -25,13 +40,23 @@ struct pixel {
 	u8 blue;
 };
 
-static int width_shrunk;
-static int height_shrunk;
-static int size  = 60;
-static int color = false;
+template <typename pixel_type = pixel, //
+          typename free_func  = decltype(&stbi_image_free)>
+struct image {
+	using ptr = std::unique_ptr<pixel_type, free_func>;
+	image(u8 *data, int height, int width, free_func ff = stbi_image_free)
+		: m_data(reinterpret_cast<pixel_type *>(data), ff), m_height(height),
+			m_width(width) {}
+	ptr m_data;
+	int m_height = 0;
+	int m_width  = 0;
+
+	pixel_type &operator[](int i) { return m_data.get()[i]; }
+};
 
 // -----------------------------------------------------------------------------
 // stole this code from here: https://alienryderflex.com/saturation.html
+[[nodiscard]]
 static constexpr auto changeSaturation(pixel p, double change) -> pixel {
 	constexpr double Pr = .299;
 	constexpr double Pg = .587;
@@ -52,34 +77,37 @@ static constexpr auto changeSaturation(pixel p, double change) -> pixel {
 }
 
 // -----------------------------------------------------------------------------
+[[nodiscard]]
 static auto load_image(const char *image_path) {
-	int  width, height, original_channels; // NOLINT
-	auto img = stbi_load(image_path, &width, &height, &original_channels, 3);
+	int   width, height, original_channels; // NOLINT
+	auto *img = stbi_load(image_path, &width, &height, &original_channels, 3);
 	if (!img) errx(2, "Failed to load image %s", image_path);
 
 	const double scale_factor = size / std::fmax(height, width);
 	width_shrunk              = width * scale_factor * 2; // Thanks bolt!
 	height_shrunk             = height * scale_factor;
 
-	pixel *img_resized = (pixel *)stbir_resize(
+	auto *img_resized = stbir_resize(
 			img, width, height, 0, nullptr, width_shrunk, height_shrunk, 0, STBIR_RGB,
 			STBIR_TYPE_UINT8, STBIR_EDGE_ZERO, STBIR_FILTER_BOX);
 
 	stbi_image_free(img);
 
-	return std::unique_ptr<pixel, decltype(&stbi_image_free)>(img_resized,
-	                                                          stbi_image_free);
+	return image((u8 *)img_resized, height, width, stbi_image_free);
 }
 
 // -----------------------------------------------------------------------------
-static auto print_ascii_art(auto &&img) {
-	constexpr char ASCIIMAP[] = "N@#W$9876543210?!abc;:+=-_,.  ";
+static auto make_ascii_art(auto &&img) {
+	constexpr char ASCIIMAP[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/"
+															"\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
 	constexpr int  num_char   = sizeof ASCIIMAP - 1;
+
+	std::stringstream art;
 
 	for (int y = 0; y != height_shrunk; y++) {
 		for (int x = 0; x != width_shrunk; x++) {
 			const int   i = y * width_shrunk + x;
-			const pixel p = changeSaturation(img.get()[i], 1.5);
+			const pixel p = changeSaturation(img[i], 1.5);
 
 			const double brightness = p.red * 0.2126 +   //
 			                          p.green * 0.7152 + //
@@ -87,13 +115,14 @@ static auto print_ascii_art(auto &&img) {
 
 			const unsigned index = unsigned(brightness * (num_char - 1) / 255);
 			if (color)
-				std::cout << std::format("\033[38;2;{:d};{:d};{:d}m{}\033[0m", //
-				                         p.red, p.green, p.blue, ASCIIMAP[index]);
-			else std::cout << ASCIIMAP[index];
+				art << std::format("\033[38;2;{:d};{:d};{:d}m{}\033[0m", //
+				                   p.red, p.green, p.blue, ASCIIMAP[index]);
+			else art << ASCIIMAP[index];
 		}
 
-		std::cout << '\n';
+		art << '\n';
 	}
+	return art.str();
 }
 
 // -----------------------------------------------------------------------------
@@ -121,17 +150,8 @@ int main(int argc, char **argv) {
 	if (optind < argc) image_path = argv[optind];
 	if (!image_path) errx(1, "Need an image");
 
-	const auto img = load_image(image_path);
-	print_ascii_art(std::move(img));
-}
-
-// -----------------------------------------------------------------------------
-// this is to give the data to emscripten so that it can resize the div
-extern "C" {
-EMSCRIPTEN_KEEPALIVE int  image_width() { return width_shrunk; }
-EMSCRIPTEN_KEEPALIVE int  image_height() { return height_shrunk; }
-EMSCRIPTEN_KEEPALIVE void set_size(int a) { size = a; }
-EMSCRIPTEN_KEEPALIVE void set_color(int z) { color = z; }
+	auto img = load_image(image_path);
+	std::cout << make_ascii_art(std::move(img));
 }
 
 // vim: ts=2
