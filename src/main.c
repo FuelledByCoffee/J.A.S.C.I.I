749906d56562c6964cb7e0c333c06b7b4fee84f0
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-08-14 19:36:13 +0200

    Const pointer to image and test larger sizes

--- src/main.c
+++ src/main.c
@@ -75,7 +75,7 @@ static image load_image(const char *image_path) {
 }
 
 // -----------------------------------------------------------------------------
-static void print_ascii_art(image *img) {
+static void print_ascii_art(const image *img) {
 	// const char ASCIIMAP[] = "N@#W$9876543210?!abc;:+=-_,.  ";
 	const char ASCIIMAP[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/"
 													"\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
@@ -127,7 +127,7 @@ int main(int argc, char **argv) {
 	if (optind < argc) image_path = argv[optind];
 	if (!image_path) errx(1, "Need image!");
 
-	image img = load_image(image_path);
+	const image img = load_image(image_path);
 	print_ascii_art(&img);
 	free(img.data);
 }
