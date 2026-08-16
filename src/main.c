cdabf6b098ac092fbfddb4e27f62d4cc09d20604
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-08-16 10:21:46 +0200

    Updates from main

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+#include <stb_image.h>
 #ifdef __EMSCRIPTEN__
 	#include <emscripten.h>
 #else
 	#define EMSCRIPTEN_KEEPALIVE
 #endif
-#include <stb_image.h>
-#include <stb_image_resize2.h>
 
 typedef uint8_t u8;
 
@@ -22,10 +22,22 @@ typedef struct {
 	u8 blue;
 } pixel;
 
-static int width_shrunk;
-static int height_shrunk;
-static int size  = 60;
-static int color = false;
+typedef struct {
+	pixel *data;
+	int    height;
+	int    width;
+} image;
+
+static int g_target_height;
+static int g_target_width = 90;
+static int g_color        = false;
+
+// -----------------------------------------------------------------------------
+// this is to give the data to emscripten so that it can resize the div
+EMSCRIPTEN_KEEPALIVE int  image_width() { return g_target_width; }
+EMSCRIPTEN_KEEPALIVE int  image_height() { return g_target_height; }
+EMSCRIPTEN_KEEPALIVE void set_size(int a) { g_target_width = a; }
+EMSCRIPTEN_KEEPALIVE void set_color(int z) { g_color = z; }
 
 // -----------------------------------------------------------------------------
 // stole this code from here: https://alienryderflex.com/saturation.html
@@ -42,45 +54,47 @@ static pixel changeSaturation(pixel p, double change) {
 	                      G * G * Pg + //
 	                      B * B * Pb);
 
-	p.red   = fmin(fabs(P + ((R)-P) * change), 255);
-	p.green = fmin(fabs(P + ((G)-P) * change), 255);
-	p.blue  = fmin(fabs(P + ((B)-P) * change), 255);
+	p.red   = (u8)fmin(fabs(P + ((R)-P) * change), 255U);
+	p.green = (u8)fmin(fabs(P + ((G)-P) * change), 255U);
+	p.blue  = (u8)fmin(fabs(P + ((B)-P) * change), 255U);
 	return p;
 }
 
 // -----------------------------------------------------------------------------
-static pixel *load_image(const char *image_path) {
-	int      width, height, original_channels; // NOLINT
-	stbi_uc *img = stbi_load(image_path, &width, &height, &original_channels, 3);
+static image load_image(const char *image_path) {
+	int original_channels, height, width; // NOLINT
+	u8 *img = stbi_load(image_path, &width, &height, &original_channels, 3);
 	if (!img) errx(1, "Failed to load image %s", image_path);
 
-	const double scale_factor = size / fmax(height, width);
-	width_shrunk              = (int)(width * scale_factor * 2);
-	height_shrunk             = (int)(height * scale_factor);
+	const double aspect_ratio           = (double)height / width;
+	const double char_aspect_adjustment = 0.55;
+	g_target_height =
+			(int)(g_target_width * aspect_ratio * char_aspect_adjustment);
 
-	pixel *img_resized =
-			stbir_resize(img, width, height, 0, NULL, width_shrunk, height_shrunk, 0,
-	                 STBIR_RGB, STBIR_TYPE_UINT8, STBIR_EDGE_ZERO,
-	                 STBIR_FILTER_BOX); // stb the goat
-	free(img);
-	return img_resized;
+	return (image){.data = (pixel *)img, .height = height, .width = width};
 }
 
 // -----------------------------------------------------------------------------
-static void print_ascii_art(pixel *img, int height, int width) {
-	const char ASCIIMAP[] = "N@#W$9876543210?!abc;:+=-_,.  ";
+static void print_ascii_art(const image *img) {
+	// const char ASCIIMAP[] = "N@#W$9876543210?!abc;:+=-_,.  ";
+	// https://paulbourke.net/dataformats/asciiart/
+	const char ASCIIMAP[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/"
+													"\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
 	const int  num_char   = sizeof ASCIIMAP - 1;
 
-	for (int y = 0; y != height; y++) {
-		for (int x = 0; x != width; x++) {
-			const int   i = y * width + x;
-			const pixel p = changeSaturation(img[i], 1.5);
+	for (int y = 0; y != g_target_height; y++) {
+		for (int x = 0; x != g_target_width; x++) {
+			const int   recalc_x = (int)((double)x / g_target_width * img->width);
+			const int   recalc_y = (int)((double)y / g_target_height * img->height);
+			const int   i        = recalc_y * img->width + recalc_x;
+			const pixel p        = changeSaturation(img->data[i], 1.5);
 
-			const double brightness = p.red * 0.2126 +   //
-			                          p.green * 0.7152 + //
-			                          p.blue * 0.0722;
-			const int index = brightness * (num_char - 1) / 255;
-			if (color)
+			const double brightness = 0.299 * p.red +   //
+			                          0.587 * p.green + //
+			                          0.114 * p.blue;
+
+			const int index = (int)((brightness / 255.0) * (num_char - 1));
+			if (g_color)
 				printf("\033[38;2;%d;%d;%dm%c\033[0m", p.red, p.green, p.blue,
 				       ASCIIMAP[index]);
 			else putchar(ASCIIMAP[index]);
@@ -90,40 +104,33 @@ static void print_ascii_art(pixel *img, int height, int width) {
 }
 
 // -----------------------------------------------------------------------------
-static const struct option long_options[] = {
-		{"version",       no_argument, NULL, 'v'},
-		{  "image", required_argument, NULL, 'i'},
-		{  "color",       no_argument, NULL, 'c'},
-		{   "size", required_argument, NULL, 's'},
-		{   "help",       no_argument, NULL, 'h'},
-		{		 NULL,								 0, NULL,   0}
-};
-
-// -----------------------------------------------------------------------------
 int main(int argc, char **argv) {
+
+	const struct option long_options[] = {
+			{"version",       no_argument, NULL, 'v'},
+			{  "image", required_argument, NULL, 'i'},
+			{  "color",       no_argument, NULL, 'c'},
+			{   "size", required_argument, NULL, 's'},
+			{   "help",       no_argument, NULL, 'h'},
+			{     NULL,								 0, NULL,   0}
+  };
+
 	int         opt        = 0;
 	const char *image_path = argv[1];
 	while ((opt = getopt_long(argc, argv, "vi:cs:h", long_options, NULL)) != -1) {
 		switch (opt) {
-			case 'c': color = 1; break;
+			case 'c': g_color = 1; break;
 			case 'i': image_path = optarg; break;
-			case 's': size = atoi(optarg); break;
+			case 's': g_target_width = atoi(optarg); break;
 			default: break;
 		}
 	}
 	if (optind < argc) image_path = argv[optind];
 	if (!image_path) errx(1, "Need image!");
 
-	pixel *img = load_image(image_path);
-	print_ascii_art(img, height_shrunk, width_shrunk);
-	free(img);
+	const image img = load_image(image_path);
+	print_ascii_art(&img);
+	free(img.data);
 }
-
-// -----------------------------------------------------------------------------
-// this is to give the data to emscripten so that it can resize the div
-EMSCRIPTEN_KEEPALIVE int  image_width() { return width_shrunk; }
-EMSCRIPTEN_KEEPALIVE int  image_height() { return height_shrunk; }
-EMSCRIPTEN_KEEPALIVE void set_size(int a) { size = a; }
-EMSCRIPTEN_KEEPALIVE void set_color(int z) { color = z; }
 
 // vim: ts=2
